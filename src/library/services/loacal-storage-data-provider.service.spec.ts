e04523fa4bf11f4c71bc24702ddfc4975fe2980e
KirillPinchukou <kirill.pinchukou@gmail.com>
2021-09-30 14:58:12 +0300

    testing addBook func

--- src/library/services/loacal-storage-data-provider.service.spec.ts
+++ src/library/services/loacal-storage-data-provider.service.spec.ts
@@ -7,8 +7,8 @@ import {compareBooks} from "./compare-books";
 
 describe('DataProviderService', () => {
   let service: LocalStorageDataProvider;
-  let book: Book;
-  let bookList:Array<Book>;
+  let addedbook: Book;
+  let expectedBooks:Array<Book>;
   let searchText:string;
 
   beforeEach(() => {
@@ -25,34 +25,38 @@ describe('DataProviderService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
   it('should return books', () => {
     localStorage.clear()
     localStorage.setItem(STORAGE_NAME, JSON.stringify(testData));
-    bookList = JSON.parse(localStorage.getItem(STORAGE_NAME)).map((obj: any) => service.mapBook(obj));
+
+    let books = JSON.parse(localStorage.getItem(STORAGE_NAME)).map((obj: any) => service.mapBook(obj));
     searchText = '';
-    for(let i = 0;i < bookList.length;i++){
-      expect(compareBooks(service.getBooks(searchText)[i],bookList[i])).toBeTruthy()
+    expectedBooks = service.getBooks(searchText);
+    for(let i = 0;i < expectedBooks.length;i++){
+      expect(compareBooks(expectedBooks[i],books[i])).toBeTruthy()
     }
     searchText = 'Idiot';
-    for(let i = 0;i < bookList.length;i++){
-      if(bookList[i].title === searchText || bookList[i].author === searchText){
-        expect(compareBooks(service.getBooks(searchText)[i],bookList[i])).toBeTruthy()
+    for(let i = 0;i < expectedBooks.length;i++){
+      if(expectedBooks[i].title === searchText || expectedBooks[i].author === searchText){
+        expect(compareBooks(books[i],expectedBooks[i])).toBeTruthy()
       }
     }
   });
   it(`"should add book`, () => {
-    book = new Book();
-    book.setId(12);
-    book.setGenre(Genre.Fantasy);
-    book.setPublicationDate(new Date());
-    book.setPublishingHouse('OZ');
-    book.setTitle('ABBA');
-    book.setAuthor('Rara');
     localStorage.clear()
     localStorage.setItem(STORAGE_NAME, JSON.stringify(data));
-    service.getBooks('');
-    service.addBook(book);
-    expect(compareBooks(service.getBooks('')[service.getBooks('').length -1],book)).toBeTruthy()
+
+    addedbook = new Book();
+    addedbook.setId(12);
+    addedbook.setGenre(Genre.Fantasy);
+    addedbook.setPublicationDate(new Date());
+    addedbook.setPublishingHouse('OZ');
+    addedbook.setTitle('ABBA');
+    addedbook.setAuthor('Rara');
+    let books = service.getBooks('');
+    service.addBook(addedbook);
+    expect(compareBooks(books[books.length -1],addedbook)).toBeTruthy()
   })
 });
 
--- src/library/services/local-storage-data-provider.service.ts
+++ src/library/services/local-storage-data-provider.service.ts
@@ -17,7 +17,6 @@ export class LocalStorageDataProvider extends DataProvider {
   }
 
   public addBook(book: Book): void {
-
     this.books.push(book);
     this.putDataToLocalStorage(JSON.stringify(this.books));
   }
