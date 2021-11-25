c10c2790870a9351b74ab664a0015ed9f3270f7b
KirillPinchukou <kirill.pinchukou@gmail.com>
2021-11-25 15:54:39 +0300

    implementing reader functionality

--- src/library/rate/rate.component.ts
+++ src/library/rate/rate.component.ts
@@ -1,5 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {DataProvider} from '../services/data-provider.service';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'rate',
@@ -8,22 +7,14 @@ import {DataProvider} from '../services/data-provider.service';
 })
 export class RateComponent  {
   rates: Array<number> = [1, 2, 3, 4, 5];
-  activeRate: Array<number>;
   @Output() rateSet = new EventEmitter<number>();
   @Input() bookRate?: number;
-
-  constructor(private dataProvider: DataProvider) {
-  }
 
   setRate(rate: number) {
     this.rateSet.emit(rate);
   }
 
   setClass(index: number): string {
-    if (index <= this.bookRate) {
-      return 'rating-area';
-    } else {
-      return 'rating';
-    }
+    return index <= this.bookRate ? 'active' : 'non-active';
   }
 }
